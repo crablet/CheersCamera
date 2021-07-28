b8ff50e8dfc1becf32e61b496f8135717fd7ecc5
crablet <love4uandgzfc@gmail.com>
2021-07-28 12:53:24 +0800

    feat: 完成刷新已拍照照片函数

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 
 import 'package:cheers_camera/main.dart';
 import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
 
 class CameraScreen extends StatefulWidget {
   const CameraScreen({Key? key}) : super(key: key);
@@ -31,6 +32,8 @@ class _CameraScreenState extends State<CameraScreen>
   double _currentZoomLevel = 1.0;
   double _currentExposureOffset = 0.0;
   FlashMode? _currentFlashMode;
+
+  List<File> allFileList = [];
 
   void onNewCameraSelected(CameraDescription cameraDescription) async {
     final previousCameraController = controller;
@@ -79,6 +82,28 @@ class _CameraScreenState extends State<CameraScreen>
     }
   }
 
+  void refreshAlreadyCapturedImages() async {
+    final directory = await getApplicationDocumentsDirectory();
+    List<FileSystemEntity> fileList = await directory.list().toList();
+    allFileList.clear();
+    List<Map<int, dynamic>> fileNames = [];
+    for (var file in fileList) {
+      if (file.path.contains(".jpg")) {
+        allFileList.add(File(file.path));
+        String name = file.path.split('/').last.split('.').first;
+        fileNames.add({0: int.parse(name), 1: file.path.split('/').last});
+      }
+    }
+
+    if (fileNames.isNotEmpty) {
+      final recentFile = fileNames.reduce((curr, next) => curr[0] > next[0] ? curr : next);
+      String recentFileName = recentFile[1];
+      _imageFile = File("${directory.path}/$recentFileName");
+    }
+
+    setState(() {});
+  }
+
   void resetCameraValues() async {
     _currentZoomLevel = 1.0;
     _currentExposureOffset = 0.0;
@@ -88,7 +113,9 @@ class _CameraScreenState extends State<CameraScreen>
   void initState() {
     // 隐藏状态栏
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    // 初始化相机
     onNewCameraSelected(cameras.first);
+    refreshAlreadyCapturedImages();
     super.initState();
   }
 
