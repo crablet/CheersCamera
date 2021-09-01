3a5cda8bcf75b28793c43fe647845efb909132c5
crablet <love4uandgzfc@gmail.com>
2021-09-01 12:53:24 +0800

    feat: 添加修改预览图片预览图片透明度的地方

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -67,6 +67,7 @@ class _CameraScreenState extends State<CameraScreen>
   bool _showFlashChoiceWidget = false;
   bool _showChangeExposureWidget = false;
   bool _showSelectPickImagePositionWidget = false;
+  bool _showChangePickImageOpacityWidget = false;
 
   // 当前屏幕上有多少手指正在触摸（触点个数），用于处理缩放
   int _pointers = 0;
@@ -77,6 +78,8 @@ class _CameraScreenState extends State<CameraScreen>
   double _currentZoomLevel = 1.0;
   double _currentExposureOffset = 0.0;
   FlashMode _currentFlashMode = FlashMode.auto;
+
+  double _currentPickImageWidgetOpacity = 0.0;
 
   List<File> allFileList = [];
 
@@ -242,7 +245,8 @@ class _CameraScreenState extends State<CameraScreen>
       physics: const BouncingScrollPhysics(),
       child: _showFlashChoiceWidget ? _buildFlashChoiceWidget() :
              _showChangeExposureWidget ? _buildChangeExposureWidget() :
-             _showSelectPickImagePositionWidget ? _buildSelectPickImagePositionWidget()
+             _showSelectPickImagePositionWidget ? _buildSelectPickImagePositionWidget() :
+             _showChangePickImageOpacityWidget ? _buildChangePickImageOpacityWidget()
              : Container()
     );
   }
@@ -270,6 +274,7 @@ class _CameraScreenState extends State<CameraScreen>
                       _showFlashChoiceWidget = !_showFlashChoiceWidget;
                       _showChangeExposureWidget = false;
                       _showSelectPickImagePositionWidget = false;
+                      _showChangePickImageOpacityWidget = false;
                     });
                   },
                 ),
@@ -286,6 +291,18 @@ class _CameraScreenState extends State<CameraScreen>
                       _showSelectPickImagePositionWidget = !_showSelectPickImagePositionWidget;
                       _showChangeExposureWidget = false;
                       _showFlashChoiceWidget = false;
+                      _showChangePickImageOpacityWidget = false;
+                    });
+                  },
+                ),
+                InkWell(
+                  child: const Icon(Icons.opacity, color: Colors.white),
+                  onTap: () {
+                    setState(() {
+                      _showChangePickImageOpacityWidget = !_showChangePickImageOpacityWidget;
+                      _showChangeExposureWidget = false;
+                      _showFlashChoiceWidget = false;
+                      _showSelectPickImagePositionWidget = false;
                     });
                   },
                 ),
@@ -296,6 +313,7 @@ class _CameraScreenState extends State<CameraScreen>
                       _showChangeExposureWidget = !_showChangeExposureWidget;
                       _showFlashChoiceWidget = false;
                       _showSelectPickImagePositionWidget = false;
+                      _showChangePickImageOpacityWidget = false;
                     });
                   },
                 ),
@@ -538,6 +556,65 @@ class _CameraScreenState extends State<CameraScreen>
           await controller!.setZoomLevel(value);
         },
       )
+    );
+  }
+
+  Widget _buildChangeOpacityWidget() {
+    return Expanded(
+      child: SizedBox(
+        width: 30,
+        child: Slider(
+          value: _currentPickImageWidgetOpacity,
+          min: 0.0,
+          max: 1.0,
+          activeColor: Colors.white,
+          inactiveColor: Colors.white30,
+          onChanged: (value) async {
+            setState(() {
+              _currentPickImageWidgetOpacity = value;
+            });
+          },
+        ),
+      )
+    );
+  }
+
+  Widget _buildShowPickImageOpacityWidget() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 5.3, right: 5.3),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            _currentPickImageWidgetOpacity.toStringAsFixed(1) + 'x',
+            style: const TextStyle(color: Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildChangePickImageOpacityWidget() {
+    return SingleChildScrollView(
+      physics: const BouncingScrollPhysics(),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                _buildShowPickImageOpacityWidget(),
+                _buildChangeOpacityWidget(),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 
