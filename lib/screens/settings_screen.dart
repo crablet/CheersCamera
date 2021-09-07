5a6da94ed1ffeca04b7cfcdd0f7c19b2af956025
crablet <love4uandgzfc@gmail.com>
2021-09-07 12:53:24 +0800

    feat: 设置页里添加关于选项

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -29,42 +29,55 @@ class _SettingsScreenState extends State<SettingsScreen> {
         children: [
           Container(
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5.3 * 2),
+              borderRadius: BorderRadius.circular(2 * 5.3),
               color: Colors.white
             ),
             child: Column(
               children: [
-                Column(
-                  children: [
-                    SwitchListTile(
-                      title: const Text("Assistive Grid"),
-                      value: _showAssistiveGridWidget,
-                      onChanged: (bool value) async {
-                        setState(() {
-                          _showAssistiveGridWidget = value;
-                        });
-                        final sp = await spFuture;
-                        sp.setBool("showAssistiveGridWidget", value);
-                      },
-                      secondary: const Icon(Icons.grid_3x3),
-                    ),
-                    Divider(
-                      color: Colors.grey[300],
-                    ),
-                    SwitchListTile(
-                      title: const Text("Spirit Level"),
-                      value: _showSpiritLevelWidget,
-                      onChanged: (bool value) async {
-                        setState(() {
-                          _showSpiritLevelWidget = value;
-                        });
-                        final sp = await spFuture;
-                        sp.setBool("showSpiritLevelWidget", value);
-                      },
-                      secondary: const Icon(Icons.border_horizontal),
-                    )
-                  ],
+                SwitchListTile(
+                  title: const Text("Assistive Grid"),
+                  value: _showAssistiveGridWidget,
+                  onChanged: (bool value) async {
+                    setState(() {
+                      _showAssistiveGridWidget = value;
+                    });
+                    final sp = await spFuture;
+                    sp.setBool("showAssistiveGridWidget", value);
+                  },
+                  secondary: const Icon(Icons.grid_3x3),
+                ),
+                Divider(
+                  color: Colors.grey[300],
+                ),
+                SwitchListTile(
+                  title: const Text("Spirit Level"),
+                  value: _showSpiritLevelWidget,
+                  onChanged: (bool value) async {
+                    setState(() {
+                      _showSpiritLevelWidget = value;
+                    });
+                    final sp = await spFuture;
+                    sp.setBool("showSpiritLevelWidget", value);
+                  },
+                  secondary: const Icon(Icons.border_horizontal),
                 )
+              ],
+            ),
+          ),
+          const SizedBox(height: 2 * 5.3),
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(2 * 5.3),
+                color: Colors.white
+            ),
+            child: Column(
+              children: [
+                ListTile(
+                  title: const Text("About"),
+                  leading: const Icon(Icons.info),
+                  onTap: () {
+                  }
+                ),
               ],
             ),
           ),
